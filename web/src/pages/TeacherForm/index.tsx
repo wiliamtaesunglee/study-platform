9d5a409f1a0475a5cbb21c98e8d7317b71ebf588
SAS EDUCACAO <wiliam.tae@gmail.com>
2020-08-07 11:56:12 -0300

    study paltform web started

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeacherForm: React.FC = () => {
+  return (
+    <div>
+      Teacher form
+    </div>
+  )
+};
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backicon from '../../assets/images/icons/back.svg';
+
+const TeacherList: React.FC = () => {
+  return (
+    <div>
+      lista de professores
+    </div>
+  )
+};
+
+export default TeacherList;
